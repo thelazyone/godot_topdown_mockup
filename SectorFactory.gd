6429067c5a0ab0f2bb90bac923bf7010ace20e57
thelazyone <giacomo.pantalone@gmail.com>
2024-11-11 08:38:34 +0100

    removed deprecated files

--- SectorFactory.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-# Preload your sector scene
-var SectorScene = preload("res://Scenes/Sector.tscn")
-
-func create_sector():
-	var sector_instance = SectorScene.instantiate()
-	sector_instance.generate_content()
-	return sector_instance
