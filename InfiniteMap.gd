97e27382db5a56b951c1eb5cc7c87cc637fb18c6
thelazyone <giacomo.pantalone@gmail.com>
2024-11-11 09:57:02 +0100

    infinite map working

--- InfiniteMap.gd
+++ InfiniteMap.gd
@@ -1,14 +1,13 @@
 extends Node2D
 
 # Adjustable parameters
-var sector_width = 1024  # Width of each sector
-var sector_height = 600  # Height of each sector (adjust as needed)
-var sectors = []         # List to keep track of active sectors
-var player_node_path = NodePath("/root/Player")  # Path to your player node
-var removal_distance = sector_width * 2          # Distance after which sectors are removed
+var sector_size : Vector2 = Vector2(800, 600)
+var sectors = []                           # List to keep track of active sectors
+var removal_distance = sector_size.x * 2    # Distance after which sectors are removed
+var sector_counter : int = 0
 
 # References to child nodes
-@onready var nav_region = $NavRegion # NavigationRegion2D node
+@onready var nav_region = $NavRegion       # NavigationRegion2D node
 
 # Camera Stuff
 @onready var camera_offset = $Camera.position.x
@@ -18,42 +17,43 @@ const CAMERA_SPEED = 800
 func move_camera():
 	$Camera.position.x = camera_offset + camera_position
 
-# Tracking Position 
 func _process(delta):
 	if Input.is_action_pressed("right"):
-			camera_position += delta * CAMERA_SPEED
-			move_camera()
+		camera_position += delta * CAMERA_SPEED
+		move_camera()
+
 	# Generate new sector if needed
-	if camera_position > (sectors.size() - 2) * sector_width:
+	if camera_position > (sector_counter - 2) * sector_size.x:
 		generate_new_sector()
+		sector_counter += 1
+		print ("sector counter is ", sector_counter)
 
 	# Remove old sectors if they're far left
-	if sectors.size() > 0 and camera_position - sectors[0]["position_x"] > removal_distance:
+	if sectors.size() > 0 and camera_position - sectors[0].position.x > removal_distance:
 		remove_old_sector()
 
 func generate_new_sector():
 	print("DEBUG: Generating new sector!")
-	var sector_position_x = sectors.size() * sector_width
-	var new_sector = {
-		"position_x": sector_position_x,
-		"buildings": [],
-		"building_nodes": [],
-		"collision_shapes": []
-	}
+	var sector_position_x = sector_counter * sector_size.x
 
-	# Generate sector metadata
+	# Create a new MapSector instance
 	var sector_instance = MapSector.new()
-	sector_instance.generate_content()
-	new_sector["buildings"] = sector_instance.buildings
-	new_sector["enemies"] = sector_instance.enemies  # Enemies will be handled by you
+	sector_instance.position.x = sector_position_x
+	sector_instance.generate_content(Vector2(sector_size))
 
 	# Create buildings and collision shapes
-	for building_rect in new_sector["buildings"]:
+	for building_rect in sector_instance.get_building_rects():
 		var nodes = add_building(building_rect, Vector2(sector_position_x, 0))
-		new_sector["building_nodes"].append(nodes["building_node"])
-		new_sector["collision_shapes"].append(nodes["collision_shape"])
+		
+		# Tracking for removal only.
+		sector_instance.building_nodes.append(nodes["building_node"])
+		sector_instance.collision_shapes.append(nodes["collision_shape"])
 
-	sectors.append(new_sector)
+	# Enemies can be handled similarly if needed
+	# for enemy_pos in sector_instance.get_enemy_positions():
+	#     spawn_enemy(enemy_pos + Vector2(sector_position_x, 0))
+
+	sectors.append(sector_instance)
 
 	# Rebake the navigation mesh
 	_rebake_navigation()
@@ -64,19 +64,32 @@ func add_building(rect: Rect2, sector_offset: Vector2) -> Dictionary:
 	building_node.color = Color(0.5, 0.5, 0.5)  # Gray color
 	building_node.position = sector_offset + rect.position
 	building_node.size = rect.size
+	var building_center = building_node.position + rect.size / 2
 	add_child(building_node)
+	
+	# Creating the navigation collider	
+	var shape = RectangleShape2D.new()
+	shape.size = rect.size
+	var collision_shape = CollisionShape2D.new()
+	collision_shape.set_shape(shape)
+	var static_body = StaticBody2D.new()
+	static_body.add_child(collision_shape)
+	nav_region.add_child(static_body)
+	static_body.set_global_position(building_center)
+
+
 
 	# Create collision shape for navigation
-	var collision_shape = CollisionPolygon2D.new()
-	var polygon = [
-		Vector2(0, 0),
-		Vector2(rect.size.x, 0),
-		Vector2(rect.size.x, rect.size.y),
-		Vector2(0, rect.size.y)
-	]
-	collision_shape.polygon = polygon
-	collision_shape.position = sector_offset + rect.position
-	nav_region.add_child(collision_shape)
+	#var collision_shape = CollisionPolygon2D.new()
+	#var polygon = [
+		#Vector2(0, 0),
+		#Vector2(rect.size.x, 0),
+		#Vector2(rect.size.x, rect.size.y),
+		#Vector2(0, rect.size.y)
+	#]
+	#collision_shape.polygon = polygon
+	#collision_shape.position = sector_offset + rect.position
+	#nav_region.add_child(collision_shape)
 
 	return {
 		"building_node": building_node,
@@ -84,23 +97,36 @@ func add_building(rect: Rect2, sector_offset: Vector2) -> Dictionary:
 	}
 
 func remove_old_sector():
-	print("Removing old sector")
-	
+
 	var old_sector = sectors.pop_front()
 
 	# Remove building nodes
-	for building_node in old_sector["building_nodes"]:
+	for building_node in old_sector.building_nodes:
 		building_node.queue_free()
 
 	# Remove collision shapes
-	for collision_shape in old_sector["collision_shapes"]:
+	for collision_shape in old_sector.collision_shapes:
 		collision_shape.queue_free()
 
 	# Rebake the navigation mesh
 	_rebake_navigation()
+	
 
 func _rebake_navigation():
 	
-	# Moving the navigation region centered where it should be, then baking
-	nav_region.position = $Camera.position
+	## Thanks to https://www.reddit.com/r/godot/comments/rzmfh3/is_there_a_way_to_get_the_rect2_of_the_camera/
+	#var camera_rect = get_canvas_transform().affine_inverse().basis_xform(get_viewport_rect().size)
+
+	
+
+	# Move the navigation region centered where it should be, then bake
+	nav_region.navigation_polygon.clear_outlines()
+	var polygon = [
+		Vector2(camera_position + 60, 0),
+		Vector2(get_viewport().size.x * 2 + camera_position, 0),
+		Vector2(get_viewport().size.x * 2 + camera_position, get_viewport().size.y),
+		Vector2(camera_position + 60, get_viewport().size.y),
+	]
+	nav_region.navigation_polygon.add_outline(polygon)
+	print("adding nav polygon at ", polygon)
 	nav_region.bake_navigation_polygon()
--- autoloads/map_sector.gd
+++ autoloads/map_sector.gd
@@ -1,12 +1,28 @@
 class_name MapSector
 extends Node2D
 
-var buildings = []  # Array of Rect2 representing building positions and sizes
-var enemies = []    # Array of Vector2 representing enemy positions
+var buildings: Array[Rect2] = []  # Array of Rect2 representing building positions and sizes
+var enemies: Array[Vector2] = []  # Array of Vector2 representing enemy positions
 
-func generate_content():
+var sector_size
+
+# Possibly TBR
+var building_nodes = []     # Nodes representing the buildings (visuals)
+var collision_shapes = []   # Collision shapes for navigation
+
+# Public Functions:
+
+func generate_content(size: Vector2):
+	sector_size = size
 	_generate_buildings()
 	_generate_enemies()
+	
+# Methods to access data
+func get_building_rects() -> Array:
+	return buildings
+
+func get_enemy_positions() -> Array:
+	return enemies
 
 func _generate_buildings():
 	# Generate Rect2 for where the buildings are and store them in "buildings"
@@ -32,25 +48,28 @@ func _random_rect(i_rect: Vector2, weight = 0) -> Vector2:
 
 func _random_point() -> Vector2:
 	# Generate a random point within the sector bounds
-	var sector_size = Vector2(1024, 600)  # Adjust as needed
 	return Vector2(randf() * sector_size.x, randf() * sector_size.y)
 
 func _populate_buildings():
-	var sections = Vector2(3, 2)
+	var sections = Vector2(2, 2)
 	var building_size = Vector2(200, 200)
 	for xi in range(int(sections.x)):
 		for yi in range(int(sections.y)):
 			# Adding a central building
 			var center = Vector2(
-				1024 / sections.x * (xi + 0.5),
-				600 / sections.y * (yi + 0.5)
+				sector_size.x / sections.x * (xi + 0.5),
+				sector_size.y / sections.y * (yi + 0.5)
 			)
+			
 			_add_building(center, _random_rect(building_size, 0.5))
+			print("creating building in ", center)
+			
 			# Adding side-buildings around it
 			for i in range(4):
 				var offset = _random_rect(building_size) - building_size / 2
 				var size = _random_rect(building_size / 3, 0.7)
 				_add_building(center + offset, size)
+				
 
 func _add_building(pos: Vector2, size: Vector2):
 	# Add the building's Rect2 to the list
